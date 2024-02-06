d23e09a844be4e979da215c647eb1661d181b659
S1m0n29 <simon.w2@gmx.net>
2024-02-06 15:53:26 +0100

    initialize portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { AtfComponent } from './atf/atf.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AtfComponent, 
+          AboutmeComponent, SkillsComponent, PortfolioComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
